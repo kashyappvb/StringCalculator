4f603d52fff9bd4a725429e64ef2b28869fe050b
kashyap.pvb <kashyappvb007@gmail.com>
2020-12-07 18:58:45 +0530

    Method taking 0 arg

---
+++ src/main/java/org/stringcalculator/stringcalculator/StringCalculator.java
@@ -0,0 +1,8 @@
+package org.stringcalculator.stringcalculator;
+
+public class StringCalculator {
+		
+	public static int add(String text){
+		return 0;
+	}
+}
---
+++ src/test/java/org/stringcalculator/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+package org.stringcalculator.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, StringCalculator.add(""));
+	}
+}
