8da74296795a3f2e0f7698ea29f13b660f075b82
kashyappvb <kashyappvb007@gmail.com>
2021-06-06 19:40:16 +0530

    Update StringCalculatorTest.java

--- src/test/java/org/stringcalculator/StringCalculatorTest.java
+++ src/test/java/org/stringcalculator/StringCalculatorTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-	
+	 
 	@Test
 	public void testEmptyString() {
 		assertEquals(0, StringCalculator.add(""));
